58df528692609764fb7425982e84053a2025f900
ElMansari-Youssef <91879610+ElMansari-Youssef@users.noreply.github.com>
2025-03-31 23:29:43 +0200

    updates

--- code.js
+++ code.js
@@ -1,7 +1,19 @@
+
+
 let menu = document.querySelector('.navbar .menu');
 let close = document.querySelector('header .mobile-menu i');
 let menuContainer = document.querySelector('.mobile-menu');
+let arrow = document.getElementById('arrow');
 
+
+let scrollEffect = () => {
+    let y = window.scrollY;
+    y >=400 ? arrow.style.opacity = '1' : arrow.style.opacity = '0';
+
+}
+
+
+window.addEventListener('scroll', scrollEffect);
 
 menu.addEventListener('click', () => {
 
